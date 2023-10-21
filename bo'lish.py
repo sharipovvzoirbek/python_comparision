4c1e908053709d165802f05846ec51b1df6708e0
sharipovvzoirbek <zabyjorda@gmail.com>
2023-10-21 13:49:30 +0500

    <

--- bo'lish.py
+++ bo'lish.py
@@ -1,7 +1,27 @@
-a = 320
-b =4
-c =210
-d = 2
-asnwer = a//b
-zaby = c//d 
-print(asnwer<zaby)
+a = 324
+b = 349
+c = 218
+d = 423
+e = 237
+a11 = b<a
+print(a11)
+a12 =b>a
+print(a12)
+a13 = b>c
+print(a13)
+a14 = e<d
+print(a14)
+a15 = a<b
+print(a15)
+a16 = c<b
+print(a16)
+a17 = d<a
+print(a17)
+a18 = b<a
+print(a18)
+a19 = c<a
+print(a19)
+a20 = d<a
+print(a20)
+a21 = d<b
+print(a21)
