d0323fc0f5daa40c180504de45bdfd81a17f1b5e
sharipovvzoirbek <zabyjorda@gmail.com>
2023-10-21 12:49:53 +0500

    operator-katta

--- ayrish.py
+++ ayrish.py
@@ -2,6 +2,25 @@ a = 345
 b = 234
 c = 453
 d = 123
-answer = a-b
-zaby = c-d 
-print(answer<zaby)
+answer = a>b
+zaby = c>b
+a11 = a>b
+a12 = c>d
+a13 = a>d
+a14 = c>b
+f11 = d<b
+f12 = a<c 
+f13 = b>d 
+f14 = c>b
+
+print(answer)
+print(zaby)
+print(a11)
+print(a12)
+print(a13)
+print(a14)
+print(f11)
+print(f12)
+print(f13)
+print(f14)
+
