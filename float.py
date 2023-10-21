ef9f33a64451e811dda775aa0ed1730f645d0840
sharipovvzoirbek <zabyjorda@gmail.com>
2023-10-21 13:51:39 +0500

    ==

--- float.py
+++ float.py
@@ -1,7 +1,25 @@
-a = 234
-b = 2
-c = 45
-d = 5
-answer = a/b
-zaby = c/d
-print(answer>zaby)
+a = 231
+b = 325
+c = 949
+d = 357
+e = 838
+a1 = a == b
+print(a1)
+a2 = b == c 
+print(a2)
+a3 = c == e 
+print(a3)
+a4 = b == d
+print(a4)
+a5 = e == a
+print(a5)
+a6 = c == b 
+print(a6)
+a7 = e == d 
+print(a7)
+a8 = c == a
+print(a8)
+a9 = c == d 
+print(a9)
+a10 = c == d
+print(a10)
