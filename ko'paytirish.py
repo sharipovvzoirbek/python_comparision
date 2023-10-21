7d6a841f05977729821c1f675ec0aa1abf8e74d1
sharipovvzoirbek <zabyjorda@gmail.com>
2023-10-21 13:52:45 +0500

    ko'paytirish.py

--- ko'paytirish.py
+++ ko'paytirish.py
@@ -1,7 +1,24 @@
-a=243
-b = 12
-c = 234
-d = 23
-answer = a*b
-zaby = c*d 
-print(answer<zaby)
+a = 68
+b = 35
+c = 86
+d = 43
+a1 = a<=b 
+print(a1)
+a2 = b <= d
+print(a2)
+a3 = c<=d
+print(a3)
+a4 = d<=a
+print(a4)
+a5 = d<=c 
+print(a5)
+a6 = d<=b
+print(a6)
+a7 = a<=c
+print(a7)
+a8 = b<=d
+print(a8)
+a9 = d<=d
+print(a9)
+a10 = c<=c 
+print(a10)
